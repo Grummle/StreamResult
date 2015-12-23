d1e4c58c1910058c38469ab897749270bf3668d2
Michael Dillon <mdillon@rlicorp.com>
2015-12-23 14:59:43 -0600

    Initial Commit

---
+++ StreamResult/StreamResult.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamResult
+{
+    public class StreamResult : FileStreamResult
+    {
+        public StreamResult(Stream fileStream, string contentType)
+            : base(fileStream, contentType)
+        {
+        }
+
+        protected override void WriteFile(HttpResponseBase response)
+        {
+            response.BufferOutput = false;
+
+            base.WriteFile(response);
+        }
+    }
+}
