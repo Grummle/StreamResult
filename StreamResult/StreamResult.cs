72da5c43f993c91ef06cc3fc9e50acd966a58754
Michael Dillon <mdillon@rlicorp.com>
2015-12-31 14:06:46 -0600

    feat(headers) - content-length, and filename

--- StreamResult/StreamResult.cs
+++ StreamResult/StreamResult.cs
@@ -6,14 +6,25 @@ namespace StreamResult
 {
     public class StreamResult : FileStreamResult
     {
-        public StreamResult(Stream fileStream, string contentType)
+        private string _filename { get; }
+        public long? _contentLength { get; set; }
+
+        public StreamResult(Stream fileStream, string contentType, string filename = null, long? contentLength = null)
             : base(fileStream, contentType)
         {
+            _filename = filename;
+            _contentLength = contentLength;
         }
 
         protected override void WriteFile(HttpResponseBase response)
         {
             response.BufferOutput = false;
+
+            if (!string.IsNullOrEmpty(_filename))
+                response.Headers.Add("Content-Disposition", $"attachment; filename=\"{_filename}\"");
+
+            if (_contentLength.HasValue)
+                response.Headers.Add("Content-Length", _contentLength.ToString());
 
             base.WriteFile(response);
         }
